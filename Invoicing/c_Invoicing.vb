89ddefede59ec92117beb8595bfd11e607c025cf
PremierJosh <baTman11>
2014-05-22 12:25:43 -0400

    begin code file for inv - need a qb session obj class though

---
+++ Invoicing/c_Invoicing.vb
@@ -0,0 +1,19 @@
+﻿Namespace Invoicing
+    Module QBMethods
+        ' 
+
+
+        ' enum status for db items
+        Enum ItemStatus As Int32
+            Ready = 5
+            Err = 6
+            Complete = 7
+        End Enum
+
+
+
+    End Module
+    Public Class c_Invoicing
+
+    End Class
+End Namespace
