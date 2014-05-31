fa494e7129cbea17ee852432efc796e6a6498b58
PremierJosh <baTman11>
2014-05-31 17:02:22 -0400

    t

---
+++ Classes/CurrencyTextBox.vb
@@ -0,0 +1,28 @@
+﻿
+Namespace Classes
+    Public Class CurrencyTextBox
+        Inherits TextBox
+
+        Private Sub Currency_TextBox_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles Me.Validating
+            If (sender.textlength > 0) Then
+                If Not Decimal.TryParse(Me.Text, _
+                                        Globalization.NumberStyles.Currency) Then
+                    'Don't let the user leave the field if the value is invalid.
+                    With Me
+                        .HideSelection = False
+                        .SelectAll()
+
+                        MessageBox.Show("Please enter a valid currency amount.", _
+                                        "Invalid Value", _
+                                        MessageBoxButtons.OK, _
+                                        MessageBoxIcon.Error)
+
+                        .HideSelection = True
+                    End With
+
+                    e.Cancel = True
+                End If
+            End If
+        End Sub
+    End Class
+End Namespace
